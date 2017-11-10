704a2fa9def02819bb7db0551351c3bc47e7e771
tomoyk <tmyk.kym@gmail.com>
2017-11-10 15:49:42 +0900

    fixed 977c a little

--- abc077/077c.py
+++ abc077/077c.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+import sys
+
 N=int(input())
 A=sorted(list( int(i) for i in input().split()))
 B=sorted(list( int(i) for i in input().split()))
 C=sorted(list( int(i) for i in input().split()))
 
 '''
+鏡もちの形状
 print(A)
 print(B)
 print(C)
@@ -13,6 +16,30 @@ print(C)
 
 count=0
 
+width=int(len(A)/2+len(A)%2)
+print(width)
+sys.exit()
+
+def getHalf(num):
+    if(num%2==0):
+        return int(num/2)
+    else:
+        return int((num+1)/2)
+
+while(True):
+    if(width<=1):
+        break
+
+    if(A[i]<=B[getHalf(width)]):
+        print("中から右を検索")
+        width=width+getHalf(width)
+    else:
+        print("左から中を検索")
+        width=getHalf(width)
+
+# 
+# 全探索
+#
 for i in range(0, len(A)):
 	for j in reversed(range(0, len(B))):
 		if(A[i]>=B[j]):
