2893c5fc021610ce76214216d5c0a9b29b1365c8
tomoyk <tmyk.kym@gmail.com>
2017-12-09 16:20:43 +0900

    fixed 077c.py(Not AC)

--- abc077/077c.py
+++ abc077/077c.py
@@ -3,54 +3,53 @@
 import sys
 
 N=int(input())
-A=sorted(list( int(i) for i in input().split()))
-B=sorted(list( int(i) for i in input().split()))
-C=sorted(list( int(i) for i in input().split()))
+A=sorted(list( int(i) for i in input().split() ))
+B=sorted(list( int(i) for i in input().split() ))
+C=sorted(list( int(i) for i in input().split() ))
 
-'''
-鏡もちの形状
 print(A)
 print(B)
 print(C)
-'''
 
-count=0
-
-width=int(len(A)/2+len(A)%2)
-print(width)
-
-def getHalf(num):
-    if(num%2==0):
-        return int(num/2)
-    else:
-        return int((num+1)/2)
-
-while(True):
-    if(width<=1):
-        break
-
-    if(A[i]<=B[getHalf(width)]):
-        print("中から右を検索")
-        width=width+getHalf(width)
-    else:
-        print("左から中を検索")
-        width=getHalf(width)
-
-sys.exit()
-
-# 
-# 全探索
 #
-for i in range(0, len(A)):
-	for j in reversed(range(0, len(B))):
-		if(A[i]>=B[j]):
-			# print("break:",A[i],B[j])
-			break
-		for k in reversed(range(0, len(C))):
-			# print("count:",count,"A:",A[i],"B:",B[j],"C:",C[k])
-			if(B[j]>=C[k]):
-				# print("break2:",A[i],B[j],C[k])
-				break
-			count+=1
+# Finding Min
+#
+def findMin():
+    for i in range(0, len(A)):
+        for j in range(0, len(B)):
+            if(A[i]>=B[j]):
+                break
 
-print(count)
+            for k in range(0, len(C)):
+                print("A:",A[i],"B:",B[j],"C:",C[k])
+                if(B[j]>=C[k]):
+                    continue
+
+                print("i",i,"j",j,"k",k)
+                return
+
+print("FindMin() ---")
+findMin()
+
+#
+# Finding Max
+#
+def findMax():
+    for i in reversed(range(0, len(A))):
+        for j in reversed(range(0, len(B))):
+            if(A[i]>=B[j]):
+                break
+
+            for k in reversed(range(0, len(C))):
+                print("A:",A[i],"B:",B[j],"C:",C[k])
+                if(B[j]<C[k]):
+                    continue
+                elif(B[j]>=C[k]):
+                    print("i",i,"j",j,"k",k+1)
+                    return
+
+    print("i",i,"j",j,"k",k)
+    return 
+
+print("FindMax() ---")
+findMax()
