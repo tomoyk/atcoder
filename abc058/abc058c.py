fabe58b0133aef6b42de3f02b6a10dc116ed401a
tomoyk <tmyk.kym@gmail.com>
2017-12-21 23:39:46 +0900

    added 058c(Not AC)

--- abc058/abc058c.py
+++ abc058/abc058c.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
-import string
-
 n=int(input())
-S=[]
-for i in range(n):
-    S.append(input())
+S=[0] * n
 
+charas=[{}] * n
 
+# 入力の選択
+for i in range(0, n):
+    # 文字の選択とカウント
+    for k in input():
+        try:
+            print("countUp:", "charas["+str(i)+"]["+k+"]")
+            charas[i][k] += 1
+            # charas[i].update({k:charas[i][k]+1})
+        except KeyError:
+            print("set", "charas["+str(i)+"]["+k+"]")
+            charas[i][k] = 1
+            # charas[i].update({k:charas[i][k]+1})
 
-#for i in string.ascii_lowercase:
-#    print(i)
-
+print(charas)
 
