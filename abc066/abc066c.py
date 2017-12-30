deea8c11d56bf46c557b54b398e59007ca327e9a
tomoyk <tmyk.kym@gmail.com>
2017-12-30 11:41:08 +0900

    fixed 066c(AC)

--- abc066/abc066c.py
+++ abc066/abc066c.py
@@ -3,20 +3,20 @@
 n=int(input())
 a=[int(i) for i in input().split()]
 
-def cal(p):
-    if(p==1 or p==2):
+def cal(p, rev):
+    if(rev):
         lst=reversed(list(enumerate(a)))
     else:
         lst=list(enumerate(a))
     for k,v in lst:
-        if(k%2==(p%2)):
+        if(k%2==p):
             print(str(v)+" ", end="")
 
 if(n%2==1):
-    cal(2)
-    cal(1)
+    cal(0, True)
+    cal(1, False)
     print()
 else:
-    cal(1)
-    cal(0)
+    cal(1, True)
+    cal(0, False)
     print()
